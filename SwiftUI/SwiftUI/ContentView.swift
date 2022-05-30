69d18bf656d681f7467575276cee3c8e0818460f
Belal <mazharul.belal@gmail.com>
2022-05-30 14:15:27 +0600

    Hello world

---
+++ SwiftUI/SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by Belal on 30/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Belal on 30/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
